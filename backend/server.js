ab991cd86114d38d57ac2c7955972dea3372d585
Andrew Murray <atmurray@bellsouth.net>
2020-09-01 20:32:44 -0400

    setting up Mongoose access to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passwordAtlas = 'User123'
+
+app.use(morgan('tiny'));
+mongoose.connect('', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => console.log('Mongoose has been connected'))
+
 
 
 
