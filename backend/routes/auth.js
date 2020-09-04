581085dd4bea3171a3d8502686bf96cb17526f79
Andrew Murray <atmurray@bellsouth.net>
2020-09-03 22:01:48 -0400

    added validation using @hapi/joi

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,26 +2,36 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string().min(7).required(),
+    email: Joi.string().min(7).email().required(),
+    password: Joi.string().min(7).required(),
+
+})
 
 
 router.post('/register', async (req, res) => {
-    console.log(req.body)
-    const { name, email, password } = req.body
-    const user = new User({
-        userName: name,
-        email: email,
-        password: password
-    });
+    const validation = schema.validate(req.body)
+    validation.error ? console.log('error') : console.log('no error')
+    res.send(validation)
 
-    try {
-        const newUser = await user.save();
-        res.send(newUsers)
-    } catch (err) {
-        res.status(400).send(err)
-        console.log(err);
-    }
 });
+// const { name, email, password } = req.body
+// const user = new User({
+//     userName: name,
+//     email: email,
+//     password: password
+// });
 
+// try {
+//     const newUser = await user.save();
+//     res.send(newUser)
+// } catch (err) {
+//     res.status(400).send(err)
+//     console.log(err);
+// }
 router.post('/login', (res, req) => {
 
 });
