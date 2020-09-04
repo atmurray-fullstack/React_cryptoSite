5ab256862c0a08467eec1a2b8701c5b3dd57a608
Andrew Murray <atmurray@bellsouth.net>
2020-09-04 13:21:58 -0400

    created validation schema to pass info and added to new validations file in routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,37 +1,33 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const { registerValidation, loginValidation } = require('./validation')
 
 
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-    name: Joi.string().min(7).required(),
-    email: Joi.string().min(7).email().required(),
-    password: Joi.string().min(7).required(),
-
-})
 
 
 router.post('/register', async (req, res) => {
-    const validation = schema.validate(req.body)
-    validation.error ? console.log('error') : console.log('no error')
-    res.send(validation)
+    const { error } = registerValidation(req.body);
+    if (error) {
+        console.log(error.details[0].message);
+        return res.status(400).send(error.details[0].message);
+    } else {
+        const { name, email, password } = req.body
+        const user = new User({
+            userName: name,
+            email: email,
+            password: password
+        });
 
+        try {
+            const newUser = await user.save();
+            res.send(newUser)
+        } catch (err) {
+            res.status(400).send(err)
+            console.log(err);
+        }
+    }
 });
-// const { name, email, password } = req.body
-// const user = new User({
-//     userName: name,
-//     email: email,
-//     password: password
-// });
 
-// try {
-//     const newUser = await user.save();
-//     res.send(newUser)
-// } catch (err) {
-//     res.status(400).send(err)
-//     console.log(err);
-// }
 router.post('/login', (res, req) => {
 
 });
---
+++ backend/routes/validation.js
@@ -0,0 +1,34 @@
+const Joi = require('@hapi/joi');
+
+
+//Register route
+const registerValidation = data => {
+
+    const schema = Joi.object({
+        name: Joi.string().min(7).required(),
+        email: Joi.string().min(7).email().required(),
+        password: Joi.string().min(7).required(),
+
+    });
+
+    return schema.validate(data)
+
+
+};
+
+const loginValidation = data => {
+
+    const schema = Joi.object({
+        name: Joi.string().min(7).required(),
+        email: Joi.string().min(7).email().required(),
+        password: Joi.string().min(7).required(),
+
+    });
+
+    return schema.validate(data)
+
+
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
