2cf5015aadb61a61b131bb08ec9c5c96956887c9
Andrew Murray <atmurray@bellsouth.net>
2020-09-08 20:52:02 -0400

    added index.html with Materialize  and Routes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+
+const
+
 
 function App() {
   return (
-    
+    <Switch>
+      <Route path='/' component={Landing} />
+      <Route path='/login' component={LoginScreen} />
+      <Route path='/register' component={Register} />
+      <Route path='/saved' component={SavedCryptos} />
+      <Route path='/home' component={Home} />
+
+
+
+
+    </Switch>
   );
 }
 
--- frontend/src/components/nav.jsx
+++ frontend/src/components/nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useLocation, NavLink } from 'react-router-dom'
 
 export default function Navbar() {
 
